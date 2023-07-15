37b8106799e03ddaa8423350404d5f3418ad10c5
shah0510 <meetshah0510@gmail.com>
2023-07-15 13:27:04 +0530

    first commit

---
+++ src/components/home/NewArrivals/NewArrivals.js
@@ -0,0 +1,121 @@
+import React from "react";
+import Slider from "react-slick";
+import Heading from "../Products/Heading";
+import Product from "../Products/Product";
+import {
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+} from "../../../assets/images/index";
+import SampleNextArrow from "./SampleNextArrow";
+import SamplePrevArrow from "./SamplePrevArrow";
+
+const NewArrivals = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="w-full pb-16">
+      <Heading heading="New Arrivals" />
+      <Slider {...settings}>
+        <div className="px-2">
+          <Product
+            _id="100001"
+            img={newArrOne}
+            productName="Z2 CPAP"
+            feat1="•  Treatment pressure display"
+            feat2="• Ramp progress indicator (if ramp set) "
+            feat3="• Ramp time remaining (if ramp set)"
+            feat4="• Battery status (if present)"
+            feat5="• AC Power indicator (if connected)"
+            feat6="•  General status display (date, mode, Auto
+              Start/Stop)"
+            feat7="• Fault indicator (if fault occurs)"
+            price=""
+            color="Black"
+            badge={true}
+            des="The Z2® CPAP machine is an ultra-small, light weight, full featured fixed-pressure cpap machine. It is the ultimate fixed pressure cpap for travelers and people with an active lifestyle."
+          />
+        </div>
+        <div className="px-2">
+          <Product
+            _id="100002"
+            img={newArrTwo}
+            productName="SOMNOtouch"
+            price=""
+            color="Black"
+            badge={true}
+            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          />
+        </div>
+        <div className="px-2">
+          <Product
+            _id="100003"
+            img={newArrThree}
+            productName="cloth Basket"
+            price="80.00"
+            color="Mixed"
+            badge={true}
+            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          />
+        </div>
+        <div className="px-2">
+          <Product
+            _id="100004"
+            img={newArrFour}
+            productName="Funny toys for babies"
+            price="60.00"
+            color="Mixed"
+            badge={false}
+            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          />
+        </div>
+        <div className="px-2">
+          <Product
+            _id="100005"
+            img={newArrTwo}
+            productName="Funny toys for babies"
+            price="60.00"
+            color="Mixed"
+            badge={false}
+            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default NewArrivals;
